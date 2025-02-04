1f0cd004a99177c248c2e4517b44ebce030f6538
ranquirosvla <rsqrodriguez@vlacademy.com>
2025-02-03 20:25:43 -0600

    Se trabaja en Calculadora.js

--- JS/PagesJS/Practica_Calculadora.js
+++ JS/PagesJS/Practica_Calculadora.js
@@ -5,5 +5,25 @@ console.log(`Funcionando Archivo: Practica_Calculadora`);
     let nombre = document.getElementById("nombre").value;
     let edad = document.getElementById("edad").value;
 
+    // Mostrar datos al usuario, Alert, Console o Inner
+
+    console.log(`Mostrar Nombre: `, nombre)
+    console.log(`Mostrar Edad: `, edad)
+
+    alert(`Nombre: ${nombre}, edad: ${edad}`)
 }
 
+function realizarSuma() {
+
+console.log("Funcionando realizarSuma")
+
+let numero1 = document.getElementById(`numero1`).value;
+let numero2 = document.getElementById(`numero2`).value;
+let suma = numero1 + numero2;
+resultado = document.getElementById("resultado").innerHTML = `${suma}`
+
+console.log(numero1);
+console.log(numero2);
+console.log(suma)
+    
+}
